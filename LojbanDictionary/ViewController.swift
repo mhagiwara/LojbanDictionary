40406a3b6071a836af10282eb06998e87d308ff3
Masato Hagiwara <masato@duolingo.com>
2017-03-06 22:08:31 -0500

    Initial commit.

---
+++ LojbanDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LojbanDictionary
+//
+//  Created by Masato Hagiwara on 3/6/17.
+//  Copyright © 2017 Masato Hagiwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
